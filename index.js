353067899f82f8f22bbdf64f9fad4dd1909d87ad
evgenii <uglanoveugene@gmail.com>
2024-05-28 09:12:45 +0500

    Add part3 exercise 3.11

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use(express.static('dist'))
+
 const morgan = require("morgan");
 
 morgan.token("request-body", (request, response) => {
